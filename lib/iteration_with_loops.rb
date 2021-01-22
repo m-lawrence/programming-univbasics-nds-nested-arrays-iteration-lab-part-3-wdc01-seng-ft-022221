4f6fc6b501d33139f5740e6e8e6bcf52325dc3e4
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-22 17:16:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,8 +1,9 @@
 def join_nested_strings(src)
   row_index = 0 
-  final_string = ""
+  
   while row_index < src.length do
     element_index = 0 
+    final_string = ""
     while element_index < src[row_index].length do 
       if src[row_index][element_index].is_a?(String) do
        final_string += src[row_index][element_index]
